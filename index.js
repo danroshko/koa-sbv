95e0ff9751a75018c108d2dd6694a2fdfd89770f
Danil Roshko <danroshko@gmail.com>
2017-07-27 11:29:58 +0500

    bug fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const validate = require('./src/validate')
-const { maybe } = require('../src/maybe')
-const { range } = require('../src/range')
+const { maybe } = require('./src/maybe')
+const { range } = require('./src/range')
 
 module.exports = exports = async function (ctx, next) {
   ctx.validate = validate
